3a0408e2b0aec88bd8c91a002935c94768d458dc
Nullexdev@gmail.com <Alimmzdev@gmail.com>
2026-06-28 14:08:45 +0330

    chore: update gradle properties, clean up dependencies and imports
    - Disable default hierarchy template for Kotlin 2.4.0 compatibility
    - Remove unused SqlDelight android/ios driver dependencies
    - Add wasm-js SqlDelight driver dependency
    - Remove unused import in FavoriteViewModel

--- feature/favorite/src/commonMain/kotlin/tech/nullexdev/cinemood/feature/favorite/presentation/FavoriteViewModel.kt
+++ feature/favorite/src/commonMain/kotlin/tech/nullexdev/cinemood/feature/favorite/presentation/FavoriteViewModel.kt
@@ -2,11 +2,10 @@ package tech.nullexdev.cinemood.feature.favorite.presentation
 
 import androidx.lifecycle.viewModelScope
 import kotlinx.collections.immutable.persistentListOf
-import tech.nullexdev.cinemood.core.presentation.mvi.MviViewModel
-import tech.nullexdev.cinemood.feature.favorite.presentation.model.FavoriteMovieItem
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
+import tech.nullexdev.cinemood.core.presentation.mvi.MviViewModel
+import tech.nullexdev.cinemood.feature.favorite.presentation.model.FavoriteMovieItem
 import tech.nullexdev.cinemood.service.domain.usecase.GetLikedVideosUseCase
 
 class FavoriteViewModel(
